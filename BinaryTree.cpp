046a30b81b571c42a03849fbd0f9e16c1ca47137
P HL <1617291164@qq.com>
2018-12-07 12:25:42 +0800

    修正完善了部分代码 了解不能通过中序建立二叉树是‘是

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,3 +1,4 @@
+
 // BinaryTree.cpp
 // 数据结构 : 二叉树
 // 二叉树的二叉链表存储表示
@@ -9,6 +10,7 @@ using namespace std;
 typedef char TElemType;
 
 ifstream inputfile; // 定义输入文件
+ifstream inputfile1;
 
 // 二叉树的二叉链表存储表示
 typedef struct BiTNode {
@@ -17,7 +19,7 @@ typedef struct BiTNode {
 }BiTNode, *BiTree;
 
 //  遍历二叉树(递归算法)
-void PreOrderTraverse(BiTree &T)
+void PreOrderTraverse(BiTree T)
 {
     // 先序遍历
     if(T)
@@ -30,7 +32,7 @@ void PreOrderTraverse(BiTree &T)
         cout<<"#";
 }
 
-void InOrderTraverse(BiTree &T)
+void InOrderTraverse(BiTree T)
 {
     // 中序遍历
     if(T)
@@ -42,8 +44,8 @@ void InOrderTraverse(BiTree &T)
     else
         cout<<"#";
 }
-
-void PostOrderTraverse(BiTree &T)
+// 后序遍历二叉树
+void PostOrderTraverse(BiTree T)
 {
     // 后序遍历
     if(T)
@@ -56,31 +58,12 @@ void PostOrderTraverse(BiTree &T)
         cout<<"#";
 }
 
-
-// 中序遍历建立二叉树
-void InOrderCreateBiTree(BiTree &T)
-{
-    char ch;
-    if(inputfile.peek() != EOF) // 读取文件指针下一位置的值
-    {
-        inputfile>>ch;
-        if(ch == '#')
-            T = NULL;
-        else
-        {
-            T = new BiTNode;
-            InOrderCreateBiTree(T->leftChild);
-            T->data = ch;
-            InOrderCreateBiTree(T->rightChild);
-        }
-    }
-
-}
-
+// 先序建立二叉树
 void PreOrderCreateBiTree(BiTree &T)
 {
     char ch;
-    cin>>ch;
+//    cin>>ch;
+    inputfile>>ch;
     if(ch == '#')
         T = NULL;
     else
@@ -92,6 +75,24 @@ void PreOrderCreateBiTree(BiTree &T)
     }
 }
 
+// 先序建立二叉树
+void PreOrderCreateBiTree1(BiTree &T)
+{
+    char ch;
+//    cin>>ch;
+    inputfile1>>ch;
+    if(ch == '#')
+        T = NULL;
+    else
+    {
+        T = new BiTNode;
+        T->data = ch;
+        PreOrderCreateBiTree1(T->leftChild);
+        PreOrderCreateBiTree1(T->rightChild);
+    }
+}
+
+// 后序建立二叉树
 void PostOrderCreateBiTree(BiTree &T)
 {
     char ch;
@@ -106,21 +107,41 @@ void PostOrderCreateBiTree(BiTree &T)
         T->data = ch;
     }
 }
+	int state = 0;
+int Judge(BiTree T, BiTree T1)
+{
+	if(T && T1)
+	{
+//		cout<<"T "<<T->data<<" ";
+//		cout<<"T1 "<<T->data<<" ";
+//		cout<<"state "<<state<<" ";
+		if((T->data == T1->data) == 0)
+			state = -1;
+		Judge(T->leftChild, T1->leftChild);
+		Judge(T->rightChild, T1->rightChild);
+	}
+	state = -1;
+	return state;
+}
 
 int main()
 {
-    BiTree T;
-    ofstream outputfile; // 定义输出文件
+    BiTree T, T1;
+    ofstream outputfile, outputfile1; // 定义输出文件
     cout<<endl<<" 写入树结点数据到 BiTree.txt文件中 "<<endl;
     outputfile.open("./BiTree.txt");
+    outputfile1.open("./BiTree1.txt");
     cout<<" 树结点数据为 ABC##DE#G##F### "<<endl;
     outputfile<<"ABC##DE#G##F###";
+    outputfile1<<"AB###CDEFGH#";
     outputfile.close(); // 关闭输出文件
+    outputfile1.close();
     cout<<" 从 BiTree.txt 文件载入树结点数据 "<<endl;
     inputfile.open("./BiTree.txt");
+    inputfile1.open("./BiTree1.txt");
 
-    cout<<" 使用树结点数据中序遍历建立二叉树"<<endl;
-    InOrderCreateBiTree(T);
+    cout<<" 使用树结点数据先序遍历建立二叉树"<<endl;
+    PreOrderCreateBiTree(T);
     inputfile.close(); // 关闭输入文件
     cout<<endl<<"---"<<endl<<" 先序遍历二叉树"<<endl<<endl<<"    ";
     PreOrderTraverse(T);
@@ -128,5 +149,22 @@ int main()
     InOrderTraverse(T);
     cout<<endl<<"---"<<endl<<" 后序遍历二叉树"<<endl<<endl<<"    ";
     PostOrderTraverse(T);
+
+    cout<<endl<<" 使用树结点数据先序遍历建立二叉树"<<endl;
+    PreOrderCreateBiTree1(T1);
+    inputfile1.close();
+    cout<<endl<<"---"<<endl<<" 先序遍历二叉树"<<endl<<endl<<"    ";
+    PreOrderTraverse(T1);
+    cout<<endl<<"---"<<endl<<" 中序遍历二叉树"<<endl<<endl<<"    ";
+    InOrderTraverse(T1);
+    cout<<endl<<"---"<<endl<<" 后序遍历二叉树"<<endl<<endl<<"    ";
+    PostOrderTraverse(T1);
+
+    cout<<endl<<"Judge"<<endl;
+    if(Judge(T, T1) == 0)
+    	cout<<"true"<<endl;
+    else
+    	cout<<"false"<<endl;
+
 
 }
