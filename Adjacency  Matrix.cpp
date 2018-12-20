9471d5a21a46a6307551ff44c91d0075639c2ce5
P HL <1617291164@qq.com>
2018-12-20 11:58:06 +0800

    邻接矩阵&最小生成树

---
+++ Adjacency  Matrix.cpp
@@ -0,0 +1,39 @@
+
+//============================================================================
+// Name        : Adjacency Matrix.cpp
+// Author      : PHL
+// Version     :
+// Copyright   : 
+// Content : 数据结构：图 邻接矩阵 (Adjacency Matrix) 最小生成树
+// Description : Hello World in C++, Ansi-style
+// Explain : 
+//			Adjacency List 邻接表
+//			Adjacency Matrix 邻接矩阵
+//			Vertex 顶点
+//			Minimum Cost Spanning Tree 最小生成树
+//			Prim 普里姆算法
+//			Kruskal 克鲁斯卡尔算法
+//			
+//============================================================================
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INTEGER 999
+#define MAX_NUMBER 10
+
+typedef struct 
+{
+	char ver[MAX_NUMBER];
+	int arc[MAX_NUMBER][MAX_NUMBER];
+};
+
+
+
+int main()
+{
+	cout<<"图的建立"<<endl;
+	return 0;
+}
