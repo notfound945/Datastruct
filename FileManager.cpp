1b5d7ad9a7206316521f0b1806ad87a19e5d3252
P HL <1617291164@qq.com>
2018-12-25 16:27:39 +0800

    重新组织数据结构

--- FileManager.cpp
+++ FileManager.cpp
@@ -4,88 +4,22 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
-typedef struct FileNode {
-	char fileName[50];
-	float fileSize;
-	struct FileNode *fileNext;
-}FileNode, *FileList;
-
-typedef struct FolderNode {
-	char folderName[50];
-	struct FolderNode *folderNext; // 下级目录文件
-	struct FolderNode *next; // 同级目录文件
-	struct FileNode *fileNext; // 同级文件
-}FolderNode, *FolderList;
-
-FolderList AllDirectory = new FolderNode; // 全局目录指针
-
-void CreateFile(FolderList &folderList, char fileName[], float fileSize)
+typedef struct FileNode
 {
-	FolderNode *p = new FolderNode; // 
-	FileNode *q = new FileNode; // 新增文件节点
-	p = folderList;
-	strcpy(q->fileName, fileName);
-	q->fileSize = fileSize;
-	q->fileNext = p->fileNext;
-	p->fileNext = q;
-}
+	char filename[50];
+	int filesize;
+}FileNode;
 
-void CreateFolder(FolderList &folderList, char folderName[])
+typedef struct DirectoryNode
 {
-	FolderNode *p = new FolderNode;
-	FolderNode *q = new FolderNode;
-	p = folderList;
-	strcpy(q->folderName, folderName);
-	q->folderNext = p->folderNext;
-	p->folderNext = q;
-}
-
-void NextDirectory(FolderList &folderList)
-{
-//	folderList->folderNext = folderList->folderNext->folderNext;
-}
-void Print(FolderList &folderList)
-{
-	FolderNode *folderTemp = new FolderNode;
-	FileNode *fileTemp = new FileNode;
-	folderTemp = folderList->folderNext;
-	fileTemp = folderList->fileNext;
-	cout<<"文件列表输出"<<endl;
-	cout<<"当前目录: ./"<<folderList->folderName<<endl;
-	cout<<"==================================================="<<endl;
-	
-	while(folderTemp)
-	{
-		cout.setf(ios::left);
-		cout.width(20);
-		cout<<"./"<<folderTemp->folderName<<endl;
-		folderTemp = folderTemp->folderNext;
-	}
-	while(fileTemp)
-	{
-		cout.setf(ios::left);
-		cout.width(20);
-		cout<<"  "<<fileTemp->fileName;
-		cout.setf(ios::left);
-		cout.width(20);
-		cout<<"  "<<fileTemp->fileSize<<endl;
-		fileTemp = fileTemp->fileNext;
-
-	}
-}
-
-void Init(FolderList &folderList)
-{
-	strcpy(folderList->folderName, "root");
-	folderList->fileNext = NULL;
-	folderList->folderNext = NULL;
-	folderList->next = NULL;
-}
+	char dirname[50];
+	FileNode *filelist;
+	DirectoryNode *directorylist;
+}DirectoryNode, DirectoryLink;
 
 // 写出文件
 void Write()
@@ -93,12 +27,12 @@ void Write()
 	ofstream outputfile("data.txt");
 	if(!outputfile)
 		cout<<"尝试打开文件流失败"<<endl;
-	outputfile<<"Something_like_you.mp3 53.32 Seve.mp3 23.4 java python";
+	outputfile<<"Something.mp3 53.32 Seve.mp3 23.4 java python";
 	outputfile.close();
 }
 
 // 读取文件
-void Read(FolderList &folderList)
+void Read()
 {
 	char fileName[50], folderName[50];
 	float fileSize;
@@ -109,37 +43,19 @@ void Read(FolderList &folderList)
 	{
 		inputfile>>fileName;
 		inputfile>>fileSize;
-		cout<<"read "<<fileName<<endl;
-		cout<<"read "<<fileSize<<endl;
-
-		CreateFile(folderList, fileName, fileSize);
-		
 		inputfile>>fileName;
 		inputfile>>fileSize;
-		cout<<"read "<<fileName<<endl;
-		cout<<"read "<<fileSize<<endl;
-		CreateFile(folderList, fileName, fileSize);
-
 		inputfile>>folderName;
-		CreateFolder(folderList, folderName);
-
 		inputfile>>folderName;
-		CreateFolder(folderList, folderName);
 	}
 }
 
 int main()
 {
-	FolderList root = new FolderNode; // 表根目录
-	AllDirectory = root;
-	Init(root);
-	Write();
-	Read(root);
-	CreateFile(root, "helloworld", 3.42);
-	Print(root);
-	NextDirectory(root);
-	//Print(root);
+	DirectoryNode *Dir = new DirectoryNode;
+	strcpy(Dir->dirname, "root");
+	strcpy(Dir->filelist->filename, "java");
+	Dir->filelist->filesize = 20;
+
 	return 0;
 }
-
-
