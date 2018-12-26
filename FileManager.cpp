f6f4a43a965c7e4ede83c04cf77661ee0efe00ae
P HL <1617291164@qq.com>
2018-12-26 16:28:35 +0800

    Update FileManager.cpp

--- FileManager.cpp
+++ FileManager.cpp
@@ -3,59 +3,92 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <fstream>
 #include <string.h>
 
 using namespace std;
 
 typedef struct FileNode
 {
-	char filename[50];
+	char filename[30];
 	int filesize;
+	FileNode *next;
 }FileNode;
 
-typedef struct DirectoryNode
+typedef struct FolderNode
 {
-	char dirname[50];
+	char foldername[20];
+	FolderNode *folderlist;
 	FileNode *filelist;
-	DirectoryNode *directorylist;
-}DirectoryNode, *DirectoryLink;
+}FolderNode, *FolderLinkList;
 
-// 写出文件
-void Write()
+
+
+void Init(FolderLinkList &folder)
 {
-	ofstream outputfile("data.txt");
-	if(!outputfile)
-		cout<<"尝试打开文件流失败"<<endl;
-	outputfile<<"Something.mp3 53.32 Seve.mp3 23.4 java python";
-	outputfile.close();
+	strcpy(folder->foldername, "root");
+	folder->filelist = NULL;
+	folder->folderlist = NULL;
 }
 
-// 读取文件
-void Read()
+void CreateFolder(FolderLinkList &folder)
 {
-	char fileName[50], folderName[50];
-	float fileSize;
-	ifstream inputfile("data.txt");
-	if(!inputfile)
-		cout<<"尝试读取文件流失败"<<endl;
-	while(!inputfile.eof())
+	FolderNode *tempNode = new FolderNode;
+	FolderNode *insertNode = new FolderNode;
+	tempNode = folder;
+
+	// 指向链表结尾处 保证每次插入元素在尾部
+	while(tempNode->folderlist)
+		tempNode = tempNode->folderlist;
+	// 从键盘中录入信息
+	cout<<"输入文件夹名"<<endl;
+	cin>>insertNode->foldername;
+	insertNode->folderlist = tempNode->folderlist;
+	tempNode->folderlist = insertNode;
+}
+
+// 建立文件
+void CreateFile(FolderLinkList &folder)
+{
+	FileNode *insertfile = new FileNode;
+	// 从键盘中录入文件信息
+	cout<<"输入文件名"<<endl;
+	cin>>insertfile->filename;
+	insertfile->next = folder->filelist;
+	folder->filelist = insertfile;
+
+}
+
+void Print(FolderLinkList &folder)
+{
+	FolderNode *tempNode = new FolderNode;
+	FileNode *tempFile = new FileNode;
+	cout<<"当前目录位置 "<<folder->foldername<<endl;
+	tempNode = folder->folderlist;
+	tempFile = folder->filelist;
+	cout<<"============"<<endl;
+	while(tempNode)
 	{
-		inputfile>>fileName;
-		inputfile>>fileSize;
-		inputfile>>fileName;
-		inputfile>>fileSize;
-		inputfile>>folderName;
-		inputfile>>folderName;
+		cout<<"./"<<tempNode->foldername<<endl;
+		
+		tempNode = tempNode->folderlist;
+	}
+	while(tempFile)
+	{
+		cout<<tempFile->filename<<endl;
+		tempFile = tempFile->next;
 	}
 }
 
 int main()
 {
-	DirectoryLink Dir = new DirectoryNode;
-	strcpy(Dir->dirname, "root");
-	strcpy(Dir->filelist->filename, "java");
-	Dir->filelist->filesize = 20;
+	FolderLinkList folder = new FolderNode;
+	Init(folder);
+	CreateFolder(folder);
+	//CreateFolder(folder);
 
+	CreateFile(folder);
+	CreateFile(folder);
+	Print(folder);
+	cout<<"下级目录"<<endl;
 	return 0;
 }
